6209568ff4e25205c780d95511da01ee258e2d3b
macf2802 <98063940+macf2802@users.noreply.github.com>
2024-06-02 07:53:38 -0300

    Initial commit

---
+++ src/main/java/net/marco/aquiver/AquiverClient.java
@@ -0,0 +1,10 @@
+package net.marco.aquiver;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AquiverClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
